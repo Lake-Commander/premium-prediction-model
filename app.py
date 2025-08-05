26f1cae68adccfcb6bec18b81ed9be84b393b4f6
Ephraim Elakhe <86500924+Lake-Commander@users.noreply.github.com>
2025-08-05 15:07:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
+import os
 
 # === Load model, scaler, and features ===
 st.markdown("🔄 Loading model, scaler, and top features...")
